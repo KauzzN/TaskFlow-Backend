128df0fc3d1ad928b7e24393114ec027715341f7
Kaynnan Jeckson <167117607+KauzzN@users.noreply.github.com>
2026-01-24 17:58:35 -0300

    Autenticação finalizada, e removendo banco de dados do repositorio

--- Backend/Backend/accounts/urls.py
+++ Backend/Backend/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from accounts import views
-from .views import login_view, signIn_view
+from .views import login_view, signIn_view, check_login_view, logout_view
 
 urlpatterns = [
     path("login/", login_view),
     path("signin/", signIn_view),
+    path("check/", check_login_view),
+    path("logout/", logout_view)
 ]
--- Backend/Backend/accounts/views.py
+++ Backend/Backend/accounts/views.py
@@ -16,13 +16,14 @@ def login_view(request):
     
     email = data.get("email")
     password = data.get("password")
+    user = data.get("username")
     
-    if not email or not password:
+    if not email or not password or not user:
         return JsonResponse({
             "error": "Dados incompletos"
         }, status=400)
         
-    user = authenticate(username=email, password=password)
+    user = authenticate(username=user, email=email, password=password)
     
     if user is None:
         return JsonResponse({
@@ -39,30 +40,89 @@ def login_view(request):
     
 @csrf_exempt
 def signIn_view(request):
+    
     if request.method != "POST":
         return JsonResponse({
             "error": "Método não permitido"
         }, status=405)
         
-    data = json.loads(request.body)
-    
+    if not request.content_type.startswith("application/json"):
+        return JsonResponse({
+            "error": "Content-type deve ser application/json"
+        }, status=400)
+        
+    try:
+        body = request.body.decode("utf-8")
+        data = json.loads(body)
+    except (UnicodeDecodeError, json.JSONDecodeError):
+        return JsonResponse({
+            "error": "Json invalido"    
+        }, status=400)
+
+
     email = data.get("email")
     password = data.get("password")
+    username = data.get("username")
     
-    if not email or not password:
+    
+    if not all([email, password, username]):
         return JsonResponse({
             "error": "Dados incompletos"
         }, status=400)
+
         
     if User.objects.filter(email=email).exists():
         return JsonResponse({
             "error": "Email já cadastrado"
-        }, status=403)
+        }, status=409)
         
-    user = User.objects.create_user(username=email, password=password)
+    if User.objects.filter(username=username).exists():
+        return JsonResponse({
+            "error": "Usuario já cadastrado"
+        }, status=409)
+        
+        
+    user = User.objects.create_user(
+        username=username,
+        email=email,
+        password=password
+    )
     
     return JsonResponse({
         "message": "Conta registrada com sucesso",
         "Id": user.id,
-        "email": user.username
-    })
+        "email": user.email,
+        "usuario": user.username
+    }, status=201)
+    
+@csrf_exempt
+def check_login_view(request):
+    
+    user = request.user
+    
+    if request.user.is_authenticated:
+        return JsonResponse({
+            "message": "Logado",
+            "ID": user.id,
+            "email": user.username
+        })
+        
+    return JsonResponse({
+        "error": "Usuario desconectado"
+    }, status=401)
+    
+@csrf_exempt
+def logout_view(request):
+    
+    user = request.user
+    
+    if request.user.is_authenticated:
+        logout(request)
+        
+        return JsonResponse({
+            "message": "Logout efetuado com sucesso"
+        })
+        
+    return JsonResponse({
+        "error": "Usuario não encontrado"
+    }, status=401)
