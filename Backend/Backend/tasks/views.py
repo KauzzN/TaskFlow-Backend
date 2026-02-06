ca28c1676009fc5ec80f401678b35f33f51c8675
KauzzN <167117607+KauzzN@users.noreply.github.com>
2026-02-06 14:08:46 -0300

    added update task feature

--- Backend/Backend/tasks/views.py
+++ Backend/Backend/tasks/views.py
@@ -111,3 +111,88 @@ def find_task_view(request, task_id):
             "created_at": task.created_at
         }
     }, status=200)
+
+@csrf_exempt
+@jwt_required
+def update_task_view(request, task_id):
+    print("CHEGOU NA UPDATE VIEW", request.method)
+    if request.method != "PATCH":
+        return JsonResponse({
+            "error": "método não permitido"
+        }, status=405)
+    
+    if "application/json" not in request.content_type:
+        return JsonResponse({
+            "error": "content_type deve ser application/json"
+        }, status=400)
+
+    try:
+        data = json.loads(request.body.decode("utf-8"))
+    except(UnicodeDecodeError, json.JSONDecodeError):
+        return JsonResponse({
+            "error": "json inválido"
+        }, status=400)
+    
+    if not data:
+        return JsonResponse({
+            "error": "nenhum dado enviado"
+        }, status=400)
+    
+    try:
+        task = Task.objects.get(id=task_id, user=request.user)
+    except Task.DoesNotExist:
+        return JsonResponse({
+            "error": "task não encontrada"
+        }, status=404)
+    
+    updated = False
+
+    if "title" in data:
+        if not isinstance(data["title"], str) or not data["title"].strip():
+            return JsonResponse({
+                "error": "title inválido"
+            }, status=400)
+        
+        if data["title"] != task.title:
+            task.title = data["title"]
+            updated = True
+
+    if "description" in data:
+        if data["description"] is not None and not isinstance(data["description"], str):
+            return JsonResponse({
+                "error": "description inválido"
+            }, status=400)
+        
+        if data["description"] != task.description:
+            task.description = data["description"] or ""
+            updated = True
+    
+    if "is_done" in data:
+        if not isinstance(data["is_done"], bool): 
+            return JsonResponse({
+                "error": "is_done deve ser boolean"
+            }, status=400)
+        
+        if data["is_done"] != task.is_done:
+            task.is_done = data["is_done"]
+            updated = True
+
+    if not updated:
+        return JsonResponse({
+            "message": "nenhuma alteração realizada"
+        }, status=200)
+
+    task.save()
+
+    return JsonResponse({
+        "message": "Task atualizada com sucesso",
+        "task": {
+            "id": task.id,
+            "title": task.title,
+            "description": task.description,
+            "is_done": task.is_done,
+            "created_at": task.created_at
+        }
+    }, status=200)
+    
+
