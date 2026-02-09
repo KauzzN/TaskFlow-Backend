8970f624e3b7cb7ddd5f9fe33246d12524033dfd
KauzzN <167117607+KauzzN@users.noreply.github.com>
2026-02-09 14:01:21 -0300

    added accounts refresh token model

--- Backend/Backend/accounts/models.py
+++ Backend/Backend/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class RefreshToken(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=255, unique=True)
+    expires_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_expiret(self):
+        return timezone.now() >= self.expires_at
